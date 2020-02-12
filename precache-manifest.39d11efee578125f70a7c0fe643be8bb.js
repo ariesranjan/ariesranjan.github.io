a4c0faeb6780a977948afce0ed7a84cc222d3fa7
Ankit Kumar Ranjan <ankranja@publicisgroupe.net>
2020-02-12 16:16:30 +0530

    added 17 guidelines

---
+++ precache-manifest.39d11efee578125f70a7c0fe643be8bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc218ee48a620d9f0f7275854119e52",
+    "url": "/static/media/demo.ccc218ee.mp4"
+  },
+  {
+    "revision": "978c7867f5877d85bd4872cac790ddaf",
+    "url": "/static/media/audio.978c7867.mp3"
+  },
+  {
+    "revision": "222c964204928721a2ce2ff1e58b35c9",
+    "url": "/static/media/accessibility.222c9642.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aebba7b728cbc42045b8",
+    "url": "/static/js/main.cfc96d5f.chunk.js"
+  },
+  {
+    "revision": "fb16100082172c2d2bd8",
+    "url": "/static/js/2.5024e983.chunk.js"
+  },
+  {
+    "revision": "aebba7b728cbc42045b8",
+    "url": "/static/css/main.5b0c63f3.chunk.css"
+  },
+  {
+    "revision": "66dc80ce5cc8e53cb807e4808488ab42",
+    "url": "/index.html"
+  }
+];
